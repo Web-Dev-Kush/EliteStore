451e960793b74469218d0ac25e26668030d17d94
Web-Dev-Kush <himanshu.kushwaha@xtendedspace.com>
2025-05-21 12:31:50 +0530

    Update db code

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -1,20 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce';
 
 if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable'
-  );
+  throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-let cached = global.mongoose;
+// Extend globalThis type
+declare global {
+  // This should only be declared once in your project
+  var mongoose: {
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+  } | undefined;
+}
+let cached = global.mongoose!;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
+async function dbConnect(): Promise<Mongoose> {
   if (cached.conn) {
     return cached.conn;
   }
@@ -24,11 +30,9 @@ async function dbConnect() {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
   }
-  
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
